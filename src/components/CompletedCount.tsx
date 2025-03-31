7f371cdc935e80381cf0c24ddaf25c60517a6084
rahuljsaliaan <rahuljsaliaan@gmail.com>
2025-03-31 13:25:20 +0530

    enhance(CompletedCount): :star: Improve the completed bad rendering issue.

--- src/components/CompletedCount.tsx
+++ src/components/CompletedCount.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 import { computed } from "@preact/signals-react";
-
 import { getTodosWithCondition } from "@app/signals";
 import { useSignals } from "@preact/signals-react/runtime";
 
 const CompletedCount: React.FC = () => {
   useSignals();
 
-  const completedCount = computed<number>(
+  const completedCount = computed(
     () => getTodosWithCondition((todo) => todo.completed).length
-  ).value;
+  );
 
-  const totalCount = computed<number>(
-    () => getTodosWithCondition(() => true).length
-  ).value;
+  const totalCount = computed(() => getTodosWithCondition(() => true).length);
 
-  if (totalCount === 0) return null;
+  if (totalCount.value === 0) return null; // .value is fine here for conditional rendering
 
   return (
     <div className="flex items-center gap-1 bg-green-200 w-fit py-0.5 px-3 font-semibold rounded-[0.15rem]">
