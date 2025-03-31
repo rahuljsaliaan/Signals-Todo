e0c1eef5013f4bd7530079ecdc068236cc4fab99
rahuljsaliaan <rahuljsaliaan@gmail.com>
2025-03-31 13:09:00 +0530

    fix(CompletedCount): :bug: CompletedCount should not be displayed when there are no completed tasks.

--- src/components/CompletedCount.tsx
+++ src/components/CompletedCount.tsx
@@ -7,6 +7,8 @@ const CompletedCount: React.FC = () => {
   const completedCount = computed<number>(
     () => getTodosWithCondition((todo) => todo.completed).length
   );
+
+  if (completedCount.value === 0) return null;
   return (
     <div className="text-white flex items-center gap-1">
       <h2>Completed</h2>{" "}
