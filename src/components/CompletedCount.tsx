b40f866e43c80d76e6cd1329cf7a8bd40cf851dc
rahuljsaliaan <rahuljsaliaan@gmail.com>
2025-03-31 13:21:31 +0530

    fix(CompletedCount): :bug: Resolve bug with completed count not updating and displaying.

--- src/components/CompletedCount.tsx
+++ src/components/CompletedCount.tsx
@@ -2,17 +2,27 @@ import React from "react";
 import { computed } from "@preact/signals-react";
 
 import { getTodosWithCondition } from "@app/signals";
+import { useSignals } from "@preact/signals-react/runtime";
 
 const CompletedCount: React.FC = () => {
+  useSignals();
+
   const completedCount = computed<number>(
     () => getTodosWithCondition((todo) => todo.completed).length
-  );
+  ).value;
 
-  if (completedCount.value === 0) return null;
+  const totalCount = computed<number>(
+    () => getTodosWithCondition(() => true).length
+  ).value;
+
+  if (totalCount === 0) return null;
+
   return (
-    <div className="text-white flex items-center gap-1">
+    <div className="flex items-center gap-1 bg-green-200 w-fit py-0.5 px-3 font-semibold rounded-[0.15rem]">
       <h2>Completed</h2>{" "}
-      <span className="text-2xl text-green-400">{completedCount}</span>
+      <span className="text-2xl text-green-600 leading-0">
+        {completedCount}
+      </span>
     </div>
   );
 };
